3de5d8201041dec02adb8e2fb6f7e33168fd83f0
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-18 19:28:14 +0200

    Redux test browser route added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
-
-
+import { TestReduxComponent } from './TestReduxComponent/TestReduxComponent';
 
 function App() {
   return (
@@ -10,6 +9,7 @@ function App() {
         {/* <Route path="/login" element={<LoginPage />}></Route> */}
         {/* <Route path="/signup" element={<SignupPage />}></Route> */}
       </Route>
+      <Route path="/redux-test" element={<TestReduxComponent />}></Route>
     </Routes>
   );
 }
