77784beabf7cc9c5c62c217f5282280a8d4252a9
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-19 23:34:26 +0200

    mock categories data deleted

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -2,44 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { getCategories } from './categoriesThunk';
 
 const initialState = {
-  categories: [
-    {
-      id: '000000000000000000000000',
-      name: 'Other expenses',
-    },
-    {
-      id: '000000000000000000000001',
-      name: 'Basic expenses',
-    },
-    {
-      id: '000000000000000000000002',
-      name: 'Products',
-    },
-    {
-      id: '000000000000000000000003',
-      name: 'Car',
-    },
-    {
-      id: '000000000000000000000004',
-      name: 'Self care',
-    },
-    {
-      id: '000000000000000000000005',
-      name: 'Child care',
-    },
-    {
-      id: '000000000000000000000006',
-      name: 'Household products',
-    },
-    {
-      id: '000000000000000000000007',
-      name: 'Education',
-    },
-    {
-      id: '000000000000000000000008',
-      name: 'Leisure',
-    },
-  ],
+  categories: [],
   errorMessage: null,
 };
 
