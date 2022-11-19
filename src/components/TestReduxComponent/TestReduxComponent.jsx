6367ae84fc269158583b2211fe056082b14ec2c4
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-20 01:32:36 +0200

    test page edited

--- src/components/TestReduxComponent/TestReduxComponent.jsx
+++ src/components/TestReduxComponent/TestReduxComponent.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   logOutUser,
@@ -12,21 +13,39 @@ export const TestReduxComponent = () => {
 
   const transactions = useSelector(transactionsSelectors.getTransactions);
 
-  const theTestDispatch = () => {
+  useEffect(() => {
+    dispatch(getTransactions(1));
+  }, [dispatch]);
+
+  const EarnDispatch = () => {
+    dispatch(
+      addTransaction({
+        income: true,
+        sum: 1200 + Math.floor(Math.random() * 10) * 100,
+        category: '6378dbbf7f1022fdac49bdf1',
+        comment: 'got paid for my job',
+      })
+    );
+  };
+
+  const SpendDispatch = () => {
     dispatch(
       addTransaction({
         income: false,
-        sum: 1200,
+        sum: 800 + Math.floor(Math.random() * 10) * 10,
         category: '6378dbbf7f1022fdac49bdf1',
-        comment: 'spending money',
+        comment: 'spending my money on stuff',
       })
     );
   };
 
   return (
     <div>
-      <button type="button" onClick={theTestDispatch}>
-        Test it
+      <button type="button" onClick={EarnDispatch}>
+        Get money
+      </button>
+      <button type="button" onClick={SpendDispatch}>
+        Spend money
       </button>
       {transactions &&
         transactions.map(e => (
