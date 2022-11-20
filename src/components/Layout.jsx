3aecbe37ff2b1c19242b4d2f0382696f9730f0ce
AlenkaNahorna <agyloma@gmail.com>
2022-11-20 19:11:37 +0100

    added Filter and pagination

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import TransactionsTable from './TransactionsTable/TransactionsTable';
 
 const Layout = () => {
   return (
     <Suspense fallback={null}>
       <div> Layout </div>
+      <TransactionsTable />
       <Outlet />
     </Suspense>
   );
--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ import { theme } from './styleConfig/theme';
 import 'antd/dist/antd.min.css';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor, getCategories } from './redux';
+import {
+  store, persistor,
+  // getCategories
+} from './redux';
 import axios from 'axios';
 
 // back-end address
-axios.defaults.baseURL = 'http://localhost:3001/api/';
+// axios.defaults.baseURL = 'http://localhost:3001/api/';
 
 // get categories on app start
-store.dispatch(getCategories());
+// store.dispatch(getCategories());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
