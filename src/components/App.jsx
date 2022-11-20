6143f5347ede78795f5f4d520acf2e439d849c29
VitaliyKhmil <hmilvitaliy@gmail.com>
2022-11-20 14:39:45 +0200

    fix conflikt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,28 @@
-import Navigation from './Navigation/Navigation';
-import Balance from './Balance/Balance';
-
-import { lazy } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { Layout } from './layout/Layout';
 import { TestReduxComponent } from './TestReduxComponent/TestReduxComponent';
+import { lazy } from 'react';
 // import { PrivateRoute, PublicRoute } from 'hocs';
-
 
 const LoginPage = lazy(() => import('pages/LoginPage'));
 const SignupPage = lazy(() => import('pages/SignupPage'));
 const DashboardPage = lazy(() => import('pages/DashboardPage'));
+// const Layout = lazy(() => import('components/layout/Layout'));
 const Home = lazy(() => import('./Home'));
-
-
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
-          <Route index element={<Navigate to="login" />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<SignupPage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          {/* <Route index path="/login" element={<PublicRoute restricted ><LoginPage/></PublicRoute>} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="login" />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        {/* <Route index path="/login" element={<PublicRoute restricted ><LoginPage/></PublicRoute>} />
         <Route path="/signup"  element={<PublicRoute restricted><SignupPage/></PublicRoute>}/>
         <Route path="/dashboard/*" element={<PrivateRoute><DashboardPage/></PrivateRoute>} /> */}
 
-          <Route path="home" element={<Home />}></Route>
-        
+        <Route path="home" element={<Home />}></Route>
       </Route>
       <Route path="/redux-test" element={<TestReduxComponent />}></Route>
     </Routes>
