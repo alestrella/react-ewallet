56abc33714dcfd676ea5b1c34482635f4f41ccc5
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-21 19:25:14 +0200

    test component edited

--- src/components/TestReduxComponent/TestReduxComponent.jsx
+++ src/components/TestReduxComponent/TestReduxComponent.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  logOutUser,
-  logInUser,
+  //   logOutUser,
+  //   logInUser,
   getTransactions,
   transactionsSelectors,
   addTransaction,
