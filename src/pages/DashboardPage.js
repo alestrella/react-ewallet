f20bb180709dd77031d34ec574b777f8eb653675
Konstantin-Kotenko <kostya.kotenkoo@gmail.com>
2022-11-22 02:31:35 +0200

    start work with dashboard

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -6,6 +6,9 @@ import ModalAddTransaction from 'components/ModalAddTransaction';
 import IconButton from 'components/IconButton';
 import { PlusOutlined } from '@ant-design/icons';
 import { Outlet } from 'react-router-dom';
+import Home from '../components/Home'
+import Balance from '../components/Balance'
+import Navigation from '../components/Navigation'
 
 const DashboardPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -15,8 +18,11 @@ const DashboardPage = () => {
       <Helmet>
         <title>Dashboard</title>
       </Helmet>
-      <div>Dashboard Page</div>
-
+      <div>
+        <Navigation/>
+        <Balance/>
+        <Home/>
+      </div>
       <IconButton onClick={() => setIsModalOpen(true)}>
         <PlusOutlined style={{ fontSize: '20px' }} />
       </IconButton>
