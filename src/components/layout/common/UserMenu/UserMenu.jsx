4f89e580139a289366ddb4802ed683f8d8681b8d
Denis <denis.tolmachev@gmail.com>
2022-11-23 20:06:04 +0200

    added user name in header

--- src/components/layout/common/UserMenu/UserMenu.jsx
+++ src/components/layout/common/UserMenu/UserMenu.jsx
@@ -1,13 +1,21 @@
-import { Menu, UserName, LogOutButton, LogOutIcon, LogOutText } from './UserMenu.styled';
+import {
+  Menu,
+  UserName,
+  LogOutButton,
+  LogOutIcon,
+  LogOutText,
+} from './UserMenu.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { logOutUser } from '../../../../redux/auth/authThunk';
+import authSelectors from '../../../../redux/auth/authSelectors';
 
 export const UserMenu = () => {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const userName = useSelector(authSelectors.getUsername);
 
   return (
     <Menu>
-      <UserName>Name {''}</UserName>
+      <UserName>{userName}</UserName>
       <LogOutButton type="button" onClick={() => dispatch(logOutUser())}>
         <LogOutIcon />
         <LogOutText>Exit</LogOutText>
