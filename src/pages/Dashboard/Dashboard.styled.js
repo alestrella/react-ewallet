bdb879d86e6514a10699180e07fe05aada7db07a
Denis <denis.tolmachev@gmail.com>
2022-11-26 18:50:23 +0200

    Changing the container, header & dashboard styles

--- src/pages/Dashboard/Dashboard.styled.js
+++ src/pages/Dashboard/Dashboard.styled.js
@@ -1,14 +1,22 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import { breakpoints } from 'styleConfig/breakpoints';
 
+export const DashbordBlur = styled.div`
+  position: relative;
+  background: ${({ theme }) => theme.colors.blur};
+  backdrop-filter: blur(25px);
+  z-index: 1;
+`;
+
 export const DashboardWrapper = styled.div`
-position:relative;
-width: 100%;
-padding-top: 12px ;
-@media screen and (${breakpoints.tablet}) {
-padding: 32px 32px 0;
-}
-@media screen and (${breakpoints.laptop}) {
-padding: 40px 16px 47px;
-}
-`
+  position: relative;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding-top: 12px;
+  @media screen and (${breakpoints.tablet}) {
+    padding: 32px 32px 0;
+  }
+  @media screen and (${breakpoints.laptop}) {
+    padding: 40px 16px 47px;
+  }
+`;
