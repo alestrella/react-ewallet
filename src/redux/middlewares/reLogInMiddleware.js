41439ae025087df2174565a263d584f642a5eea7
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-11-29 22:18:46 +0200

    token check

--- src/redux/middlewares/reLogInMiddleware.js
+++ src/redux/middlewares/reLogInMiddleware.js
@@ -1,9 +1,11 @@
 import { logInUser, reLogInUser } from '../auth/authThunk';
 
 export const reLogInMiddleware = store => next => action => {
+  console.log('refreshToken right now is', store.getState().auth.refreshToken);
   if (
     (action.type === 'persist/REHYDRATE' && action.payload?.refreshToken) ||
-    action.type === logInUser.rejected.type
+    (action.type === logInUser.rejected.type &&
+      store.getState().auth.refreshToken)
   ) {
     store.dispatch(reLogInUser(action.payload.refreshToken));
   }
