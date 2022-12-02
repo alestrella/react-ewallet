1767017dd28af8a8e0b1f67a48104d3b4efd4de0
Alexandra Sabatin <s4batin@gmail.com>
2022-12-02 16:55:14 +0200

    fix input floating number

--- src/components/Navigation/Navigation.style.js
+++ src/components/Navigation/Navigation.style.js
@@ -29,7 +29,7 @@ export const NavigLink = styled(NavLink)`
   &:focus {
     font-weight: ${p => p.theme.fontWeights.bold};
     color: ${p => p.theme.colors.black};
-    text-decoration: underline;
+    /* text-decoration: underline; */
   }
   @media screen and (${breakpoints.tablet}) {
     &:not(:last-child) {
@@ -40,14 +40,13 @@ export const NavigLink = styled(NavLink)`
     color: ${p => p.theme.colors.black};
     font-weight: ${p => p.theme.fontWeights.bold};
   }
-  
 `;
 
 export const NavItem = styled.li`
   display: flex;
   margin-left: auto;
   margin-right: auto;
- 
+
   @media screen and (${breakpoints.tablet}) {
     display: block;
     margin-left: 0;
@@ -67,8 +66,7 @@ export const NavText = styled.span`
   color: ${p => p.theme.colors.black};
   font-weight: ${p => p.theme.fontWeights.normal};
   line-height: ${p => p.theme.lineHeights.body};
-  &:hover
-   {
+  &:hover {
     color: ${p => p.theme.colors.black};
     font-weight: ${p => p.theme.fontWeights.bold};
   }
@@ -107,7 +105,7 @@ export const IconHome = styled(FaHome)`
       transition: color 250ms linear;
       background-color: ${p => p.theme.colors.secondary};
       filter: drop-shadow(0px 3px 10px rgba(74, 86, 226, 0.5));
-    }  
+    }
   }
   @media screen and (${breakpoints.laptop}) {
     &:hover,
@@ -116,7 +114,7 @@ export const IconHome = styled(FaHome)`
       transition: color 250ms linear;
       background-color: ${p => p.theme.colors.secondary};
       filter: drop-shadow(0px 3px 10px rgba(74, 86, 226, 0.5));
-    }    
+    }
   }
 `;
 export const IconGraph = styled(SlGraph)`
@@ -163,15 +161,15 @@ export const IconCurrency = styled(MdOutlineAttachMoney)`
   @media screen and (${breakpoints.tablet}) {
     &:hover,
     &:focus,
-    &:active {      
-      background-color: ${p => p.theme.colors.secondary};      
+    &:active {
+      background-color: ${p => p.theme.colors.secondary};
     }
   }
   @media screen and (${breakpoints.laptop}) {
     &:hover,
     &:focus,
-    &:active {     
-      background-color: ${p => p.theme.colors.secondary};      
+    &:active {
+      background-color: ${p => p.theme.colors.secondary};
     }
   }
 `;
